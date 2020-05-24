81e85d49dc18c1c1e189bdafc0676933d9fa14e2
Aurelian Hermand <aurel@hermand.de>
2020-05-24 21:44:47 +0200

    "Invalid Date" error with this.date

--- carbon.js
+++ carbon.js
@@ -5,46 +5,39 @@
 
 class Carbon extends Date {
     constructor() {
-        super(...arguments);
-
         let arg0 = arguments[0];
-
-        this.date = new Date(); // default
+        let date = new Date(); // default
 
         if (typeof arg0 == 'number') {
-            this.date = new Date(arg0);
+            date = new Date(arg0);
         }
         if (typeof arg0 == 'string') {
-            let time = this.strtotime(arg0);
-            this.date = new Date(time * 1000);
+            let str = arg0;
+            let now = +date / 1000;
+            // https://locutus.io/php/datetime/strtotime/
+            let time = window.strtotime(str, now);
+            date = new Date(time * 1000);
         }
         if (typeof arg0 == 'object' && arg0.constructor.name == 'Carbon') {
             return arg0;
         }
 
+        super(date);
+
         return this;
-        
     }
   
     modify(str) {
-        let now = +(this.date) / 1000;
+        let current = +(this) / 1000;
         // https://locutus.io/php/datetime/strtotime/
-        let time = window.strtotime(str, now);
-        let date = new Date(time * 1000);
-        this.date = date;
+        let time = window.strtotime(str, current);
+        this.setTime(time * 1000);
         return this;
-    }
-
-    strtotime(str) {
-        let now = +(new Date()) / 1000;
-        // https://locutus.io/php/datetime/strtotime/
-        let time = window.strtotime(str, now);
-        return time;
     }
 
     format(format) {
         // https://locutus.io/php/datetime/date/
-        let str = window.date(format, +this.date / 1000);
+        let str = window.date(format, +this / 1000);
         return str;
     }
 }
